913ac56196ab0e91ec930193825cd4ae1f7a4325
girikrishav <girikrishva@gmail.com>
2016-11-06 18:50:49 +0530

    Made lookup_types and lookups columns not null as needed.

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,6 +1,10 @@
 class Lookup < ActiveRecord::Base
   belongs_to :lookup_type, :class_name => 'LookupType', :foreign_key => :lookup_type_id
 
+  validates :value, presence: true
+  validates :rank, presence: true
+  validates :lookup_type_id, presence: true
+
   validates_uniqueness_of :value, scope: [:lookup_type_id]
   validates_uniqueness_of :rank, scope: [:lookup_type_id]
 end
--- app/models/lookup_type.rb
+++ app/models/lookup_type.rb
@@ -1,5 +1,6 @@
 class LookupType < ActiveRecord::Base
   has_many :lookups, :class_name => 'Lookup'
 
+  validates :name, presence: true
   validates_uniqueness_of :name
 end
