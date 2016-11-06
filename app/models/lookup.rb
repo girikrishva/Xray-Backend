345676e8d45bacf21f4a1a4d3f223c3518d45f62
girikrishav <girikrishva@gmail.com>
2016-11-06 15:27:46 +0530

    Added uniqueness validations.

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,5 +1,6 @@
 class Lookup < ActiveRecord::Base
-
-
   belongs_to :lookup_type, :class_name => 'LookupType', :foreign_key => :lookup_type_id
+
+  validates_uniqueness_of :value, scope: [:lookup_type_id]
+  validates_uniqueness_of :rank, scope: [:lookup_type_id]
 end
--- app/models/lookup_type.rb
+++ app/models/lookup_type.rb
@@ -1,5 +1,5 @@
 class LookupType < ActiveRecord::Base
-
-
   has_many :lookups, :class_name => 'Lookup'
+
+  validates_uniqueness_of :name
 end
