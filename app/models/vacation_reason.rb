7f9fb4d23c4dea548623a2704cec7fdd325ef570
girikrishav <girikrishva@gmail.com>
2016-11-07 21:51:38 +0530

    Fixed Business Unit bug in vacation_reasons.

--- app/models/vacation_reason.rb
+++ app/models/vacation_reason.rb
@@ -6,15 +6,5 @@ class VacationReason < ActiveRecord::Base
   validates :days_allowed, presence: true
   validates :lookup, presence: true
 
-  before_create :lookup_id_is_a_business_unit
-
   validates_uniqueness_of :code, scope: [:as_on]
-
-  def lookup_id_is_a_business_unit
-    if !LookupType.find(Lookup.find(self.lookup_id).lookup_type_id).name = 'Business Units'
-      false
-    else
-      true
-    end
-  end
 end
