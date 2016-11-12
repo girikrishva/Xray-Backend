b360dc751a43c148b185e6064c4368226ed40b14
girikrishav <girikrishva@gmail.com>
2016-11-12 13:57:12 +0530

    Further enhancements to authorization.

--- app/helpers/active_admin_helper.rb
+++ app/helpers/active_admin_helper.rb
@@ -7,7 +7,7 @@ module ActiveAdminHelper
 
   def is_authorized?(allowed_roles)
     current_role = Role.find(current_admin_user.role_id).name
-    if allowed_roles.include?(current_role)
+    if allowed_roles.blank? or allowed_roles.include?(current_role)
       return true
     else
       return false
