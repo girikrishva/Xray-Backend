9bfcb330164dde7918d0ba32bf970a18d51f6e63
girikrishav <girikrishva@gmail.com>
2016-11-13 07:50:19 +0530

    Added is_not_authorized functionality.

--- app/helpers/active_admin_helper.rb
+++ app/helpers/active_admin_helper.rb
@@ -5,19 +5,33 @@ module ActiveAdminHelper
     # nothing ...
   end
 
-  def is_authorized?(allowed_roles)
-    if allowed_roles.blank?
+  def is_authorized?(allowed_roles, disallowed_roles = [])
+    if allowed_roles.blank? and disallowed_roles.blank?
       return true
     end
     allowed_roles.each do |allowed_role|
       if Role.where(name: allowed_role).first.id == current_admin_user.role_id
-        return true
+        return is_not_authorized(disallowed_roles) & true
       end
       allowed_role_ancestry = Role.where(name: allowed_role).first.ancestor_ids
       if allowed_role_ancestry.include?(current_admin_user.role_id)
-        return true
+        return is_not_authorized(disallowed_roles) & true
       end
     end
     return false
+  end
+
+  private
+
+  def is_not_authorized(disallowed_roles)
+    if disallowed_roles.blank?
+      return true
+    end
+    disallowed_roles.each do |disallowed_role|
+      if Role.where(name: disallowed_role).first.id == current_admin_user.role_id
+        return false
+      end
+    end
+    return true
   end
 end
