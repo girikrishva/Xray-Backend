313b1695aa0fb9e75cd9f61c73717bd5a5453f22
girikrishav <girikrishva@gmail.com>
2016-11-14 15:59:00 +0530

    Customized message for is_active.

--- app/admin/error.rb
+++ app/admin/error.rb
@@ -2,19 +2,17 @@ ActiveAdmin.register_page "Error" do
   menu false
 
   action_item only: :index do |resource|
-    link_to "Back", :back
+    if params.has_key?(:back_path)
+      link_to "Back", params[:back_path].to_s
+    else
+      link_to "Back", :back
+    end
   end
 
   content title: proc { "Error" } do
     div class: "blank_slate_container", id: "error_default_message" do
       span class: "blank_slate" do
       end
-    end
-  end
-
-  controller do
-    before_filter do |c|
-      c.send(:is_resource_authorized?, ["User"])
     end
   end
 end
