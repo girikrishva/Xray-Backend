0d20b41ce4e1f085dd416b8e9e88deae39684e66
girikrishav <girikrishva@gmail.com>
2016-11-22 17:28:48 +0530

    Added table for staffing_requirements.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121040148) do
+ActiveRecord::Schema.define(version: 20161122114718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -292,6 +292,7 @@ SELECT lookups.id,
     t.integer  "delivery_manager_id",   :null=>false, :index=>{:name=>"index_projects_on_delivery_manager_id"}, :foreign_key=>{:references=>"admin_users", :name=>"fk_projects_delivery_manager_id", :on_update=>:no_action, :on_delete=>:no_action}
     t.integer  "pipeline_id",           :null=>false, :index=>{:name=>"index_projects_on_pipeline_id"}, :foreign_key=>{:references=>"pipelines", :name=>"fk_projects_pipeline_id", :on_update=>:no_action, :on_delete=>:no_action}
     t.integer  "sales_person_id",       :null=>false, :index=>{:name=>"index_projects_on_sales_person_id"}, :foreign_key=>{:references=>"admin_users", :name=>"fk_projects_admin_user_id", :on_update=>:no_action, :on_delete=>:no_action}
+    t.string   "name",                  :null=>false
   end
 
   create_table "projects_audits", force: :cascade do |t|
@@ -312,6 +313,7 @@ SELECT lookups.id,
     t.integer  "pipeline_id",           :null=>false, :index=>{:name=>"index_projects_audits_on_pipeline_id"}, :foreign_key=>{:references=>"pipelines", :name=>"fk_projects_audits_pipeline_id", :on_update=>:no_action, :on_delete=>:no_action}
     t.integer  "sales_person_id",       :null=>false, :index=>{:name=>"index_projects_audits_on_sales_person_id"}, :foreign_key=>{:references=>"admin_users", :name=>"fk_projects_audits_admin_user_id", :on_update=>:no_action, :on_delete=>:no_action}
     t.integer  "project_id",            :null=>false, :index=>{:name=>"index_projects_audits_on_project_id"}, :foreign_key=>{:references=>"projects", :name=>"fk_projects_audits_project_id", :on_update=>:no_action, :on_delete=>:no_action}
+    t.string   "name",                  :null=>false
   end
 
   create_table "resources", force: :cascade do |t|
@@ -338,6 +340,20 @@ SELECT lookups.id,
   WHERE (((lookup_types.name)::text = 'Skills'::text) AND (lookups.lookup_type_id = lookup_types.id))
   ORDER BY lookups.rank
   END_VIEW_SKILLS
+
+  create_table "staffing_requirements", force: :cascade do |t|
+    t.date     "start_date",      :null=>false
+    t.date     "end_date",        :null=>false
+    t.integer  "number_required", :null=>false
+    t.float    "hours_per_day",   :null=>false
+    t.boolean  "fulfilled"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "pipeline_id",     :null=>false, :index=>{:name=>"index_staffing_requirements_on_pipeline_id"}, :foreign_key=>{:references=>"pipelines", :name=>"fk_staffing_requirements_pipeline_id", :on_update=>:no_action, :on_delete=>:no_action}
+    t.integer  "skill_id",        :null=>false, :index=>{:name=>"index_staffing_requirements_on_skill_id"}, :foreign_key=>{:references=>"lookups", :name=>"fk_staffing_requirements_skill_id", :on_update=>:no_action, :on_delete=>:no_action}
+    t.integer  "designation_id",  :null=>false, :index=>{:name=>"index_staffing_requirements_on_designation_id"}, :foreign_key=>{:references=>"lookups", :name=>"fk_staffing_requirements_designation_id", :on_update=>:no_action, :on_delete=>:no_action}
+    t.string   "comments"
+  end
 
   create_view "vacation_codes", <<-'END_VIEW_VACATION_CODES', :force => true
 SELECT lookups.id,
