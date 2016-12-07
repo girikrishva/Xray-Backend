700f113e1e08e7957955826c2e0f748d594ddf05
girikrishav <girikrishva@gmail.com>
2016-12-07 11:31:28 +0530

    Fixed several bugs in payment_lines.

--- app/models/payment_line.rb
+++ app/models/payment_line.rb
@@ -12,21 +12,7 @@ class PaymentLine < ActiveRecord::Base
   validates_uniqueness_of :payment_header_id, scope: [:payment_header_id, :invoice_line_id]
   validates_uniqueness_of :invoice_line_id, scope: [:payment_header_id, :invoice_line_id]
 
-  after_create :update_header_amount
-  after_update :update_header_amount
-  after_destroy :update_header_amount
-
   def payment_header_name
     self.payment_header.payment_header_name
-  end
-
-  def update_header_amount
-    payment_header = PaymentHeader.where(id: self.payment_header_id).first
-    payment_header.header_amount = PaymentLine.where(payment_header_id: payment_header.id).sum(:line_amount)
-    payment_header.save
-  end
-
-  def unapplied_amount
-    self.invoice_line.line_amount - self.line_amount
   end
 end
