631eba0333911395ba5391bb45f7e997e4b2d45c
girikrishav <girikrishva@gmail.com>
2016-12-19 15:41:40 +0530

    Fixed scope and filter bug in staffing_requirements.

--- app/admin/staffing_requirement.rb
+++ app/admin/staffing_requirement.rb
@@ -21,13 +21,14 @@ ActiveAdmin.register StaffingRequirement do
 
   config.clear_action_items!
 
-  scope I18n.t('label.active'), default: true do |resources|
-    StaffingRequirement.without_deleted.where('pipeline_id = ?', session[:pipeline_id]).order('start_date asc, end_date asc')
-  end
-
-  scope I18n.t('label.deleted'), default: false do |resources|
+  scope I18n.t('label.deleted'), if: proc { current_admin_user.role.super_admin }, default: false do |resources|
     StaffingRequirement.only_deleted.where('pipeline_id = ?', session[:pipeline_id]).order('start_date asc, end_date asc')
   end
+
+  action_item only: :index do |resource|
+    link_to I18n.t('label.all'), admin_staffing_requirements_path(pipeline_id: params[:pipeline_id])
+  end
+
   action_item only: :index do |resource|
     link_to I18n.t('label.new'), new_admin_staffing_requirement_path(pipeline_id: session[:pipeline_id]) if session.has_key?(:pipeline_id)
   end
@@ -121,6 +122,8 @@ ActiveAdmin.register StaffingRequirement do
         params.merge! extra_params
       end
     end
+
+    before_filter :skip_sidebar!, if: proc { params.has_key?(:scope) }
 
     def scoped_collection
       StaffingRequirement.includes [:pipeline, :skill, :designation]
