791d82f5e9580b2f0107761d5b4ff92a16694740
girikrishav <girikrishva@gmail.com>
2016-12-19 14:32:35 +0530

    Fixed scope and filter bug in project_types.

--- app/admin/project_type.rb
+++ app/admin/project_type.rb
@@ -20,12 +20,12 @@ ActiveAdmin.register ProjectType do
 
   config.clear_action_items!
 
-  scope I18n.t('label.active'), default: true do |resources|
-    ProjectType.without_deleted
+  scope I18n.t('label.deleted'), if: proc { current_admin_user.role.super_admin }, default: false do |resources|
+    ProjectType.only_deleted
   end
 
-  scope I18n.t('label.deleted'), default: false do |resources|
-    ProjectType.only_deleted
+  action_item only: :index, if: proc { current_admin_user.role.super_admin } do |resource|
+    link_to I18n.t('label.all'), admin_project_types_path
   end
 
   action_item only: :index do |resource|
@@ -83,6 +83,8 @@ ActiveAdmin.register ProjectType do
     before_filter do |c|
       c.send(:is_resource_authorized?, [I18n.t('role.executive')])
     end
+
+    before_filter :skip_sidebar!, if: proc { params.has_key?(:scope) }
 
     def scoped_collection
       ProjectType.includes [:project_type_code, :business_unit]
