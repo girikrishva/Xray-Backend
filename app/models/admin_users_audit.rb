d5848383b0f4f7ba15e8eae3c9d4d21285ef0957
girikrishav <girikrishva@gmail.com>
2017-01-28 18:40:05 +0530

    'Resource Performance' report APIs (interim).

--- app/models/admin_users_audit.rb
+++ app/models/admin_users_audit.rb
@@ -15,4 +15,143 @@ class AdminUsersAudit < ActiveRecord::Base
   def self.latest(admin_user_id, as_on)
     AdminUsersAudit.where('admin_user_id = ? and created_at <= ?', admin_user_id, as_on).order('created_at').last
   end
+
+  def self.active_users(as_on)
+    as_on = (as_on.nil?) ? Date.today : Date.parse(as_on.to_s)
+    ids = []
+    AdminUsersAudit.pluck('admin_user_id').uniq.each do |aua_id|
+      if (aua_latest = AdminUsersAudit.latest(aua_id, as_on)).active
+        ids << aua_latest.id
+      end
+    end
+    AdminUsersAudit.where('id in (?)', ids).order('name')
+  end
+
+  def self.inactive_users(as_on)
+    as_on = (as_on.nil?) ? Date.today : Date.parse(as_on.to_s)
+    ids = []
+    AdminUsersAudit.pluck('admin_user_id').uniq.each do |aua_id|
+      if !(aua_latest = AdminUsersAudit.latest(aua_id, as_on)).active
+        ids << aua_latest.id
+      end
+    end
+    AdminUsersAudit.where('id in (?)', ids).order('name')
+  end
+
+  def self.all_users(as_on)
+    as_on = (as_on.nil?) ? Date.today : Date.parse(as_on.to_s)
+    ids = []
+    AdminUsersAudit.pluck('admin_user_id').uniq.each do |aua_id|
+      aua_latest = AdminUsersAudit.latest(aua_id, as_on)
+      ids << aua_latest.id
+    end
+    AdminUsersAudit.where('id in (?)', ids).order('name')
+  end
+
+  def self.active_users_outflow(from_date, to_date)
+    from_date = Date.parse(from_date)
+    to_date = Date.parse(to_date)
+    with_details = (with_details.to_s == 'true') ? true : false
+    data = {}
+    lower_date = from_date
+    (from_date..to_date).each do |d|
+      if (d - lower_date) > 22
+        lower_date = Date.parse(d.year.to_s + '-' + d.month.to_s + '-' + '1'.to_s)
+        next
+      end
+      month_year_key = AdminUsersAudit.month_year(d)
+      AdminUsersAudit.active_users(d).each do |aua|
+        user_key = aua.admin_user_id
+        if !data.has_key?(user_key)
+          data[user_key] = {}
+        else
+          data[user_key]['name'] = aua.name
+          data[user_key]['business_unit'] = aua.business_unit.name
+          data[user_key]['date_of_joining'] = aua.date_of_joining.to_s
+          data[user_key]['date_of_leaving'] = aua.date_of_leaving.to_s
+          data[user_key]['active'] = aua.active.to_s
+          if !data[user_key].has_key?(month_year_key)
+            data[user_key][month_year_key] = aua.cost_rate * Rails.configuration.max_work_hours_per_day
+          else
+            data[user_key][month_year_key] += (aua.cost_rate * Rails.configuration.max_work_hours_per_day)
+          end
+        end
+      end
+    end
+    data
+  end
+
+  def self.inactive_users_outflow(from_date, to_date)
+    from_date = Date.parse(from_date)
+    to_date = Date.parse(to_date)
+    with_details = (with_details.to_s == 'true') ? true : false
+    data = {}
+    lower_date = from_date
+    (from_date..to_date).each do |d|
+      if (d - lower_date) > 22
+        lower_date = Date.parse(d.year.to_s + '-' + d.month.to_s + '-' + '1'.to_s)
+        next
+      end
+      month_year_key = AdminUsersAudit.month_year(d)
+      AdminUsersAudit.inactive_users(d).each do |aua|
+        user_key = aua.admin_user_id
+        if !data.has_key?(user_key)
+          data[user_key] = {}
+        else
+          data[user_key]['name'] = aua.name
+          data[user_key]['business_unit'] = aua.business_unit.name
+          data[user_key]['date_of_joining'] = aua.date_of_joining.to_s
+          data[user_key]['date_of_leaving'] = aua.date_of_leaving.to_s
+          data[user_key]['active'] = aua.active.to_s
+          if !data[user_key].has_key?(month_year_key)
+            data[user_key][month_year_key] = aua.cost_rate * Rails.configuration.max_work_hours_per_day
+          else
+            data[user_key][month_year_key] += (aua.cost_rate * Rails.configuration.max_work_hours_per_day)
+          end
+        end
+      end
+    end
+    data
+  end
+
+  def self.all_users_outflow(from_date, to_date)
+    from_date = Date.parse(from_date)
+    to_date = Date.parse(to_date)
+    with_details = (with_details.to_s == 'true') ? true : false
+    data = {}
+    lower_date = from_date
+    (from_date..to_date).each do |d|
+      if (d - lower_date) > 22
+        lower_date = Date.parse(d.year.to_s + '-' + d.month.to_s + '-' + '1'.to_s)
+        next
+      end
+      month_year_key = AdminUsersAudit.month_year(d)
+      AdminUsersAudit.all_users(d).each do |aua|
+        user_key = aua.admin_user_id
+        if !data.has_key?(user_key)
+          data[user_key] = {}
+        else
+          data[user_key]['name'] = aua.name
+          data[user_key]['business_unit'] = aua.business_unit.name
+          data[user_key]['date_of_joining'] = aua.date_of_joining.to_s
+          data[user_key]['date_of_leaving'] = aua.date_of_leaving.to_s
+          data[user_key]['active'] = aua.active.to_s
+          if !data[user_key].has_key?(month_year_key)
+            data[user_key][month_year_key] = aua.cost_rate * Rails.configuration.max_work_hours_per_day
+          else
+            data[user_key][month_year_key] += (aua.cost_rate * Rails.configuration.max_work_hours_per_day)
+          end
+        end
+      end
+    end
+    data
+  end
+
+
+  private
+
+  def self.month_year(as_on)
+    as_on = (as_on.nil?) ? Date.today : Date.parse(as_on.to_s)
+    as_on.strftime('%B').upcase.slice(0, 3) + '-' + as_on.year.to_s
+  end
 end
