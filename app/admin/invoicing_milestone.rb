fdf25d6d352c0daf9fdf26e02a259895d7d2cdb0
girikrishav <girikrishva@gmail.com>
2017-01-29 14:34:17 +0530

    'Milestones' report APIs (interim).

--- app/admin/invoicing_milestone.rb
+++ app/admin/invoicing_milestone.rb
@@ -178,6 +178,14 @@ ActiveAdmin.register InvoicingMilestone do
       InvoicingMilestone.restore(params[:id])
       redirect_to admin_invoicing_milestones_path(project_id: session[:project_id])
     end
+
+    def invoicing_milestones
+      as_on = params[:as_on]
+      due_status = params[:due_status]
+      completion_status = params[:completion_status]
+      result = InvoicingMilestone.invoicing_milestones(as_on, due_status, completion_status)
+      render json: result
+    end
   end
 
   form do |f|
--- app/models/invoicing_milestone.rb
+++ app/models/invoicing_milestone.rb
@@ -34,4 +34,155 @@ class InvoicingMilestone < ActiveRecord::Base
   def self.invoicing_milestones_for_project(project_id)
     InvoicingMilestone.where(project_id: project_id).order(:due_date)
   end
+
+  def self.invoicing_milestones(as_on, due_status, completion_status)
+    as_on = (as_on.nil?) ? Date.today : Date.parse(as_on.to_s)
+    data = []
+    if due_status.upcase == 'DUE' and completion_status.upcase == 'INCOMPLETE'
+      InvoicingMilestone.where('due_date <= ? and completion_date is null', as_on).order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    elsif due_status.upcase == 'DUE' and completion_status.upcase == 'COMPLETE'
+      InvoicingMilestone.where('due_date <= ? and completion_date is not null', as_on).order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    elsif due_status.upcase == 'DUE' and completion_status.upcase == 'ALL'
+      InvoicingMilestone.all.order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    elsif due_status.upcase == 'FUTURE' and completion_status.upcase == 'INCOMPLETE'
+      InvoicingMilestone.where('due_date > ? and completion_date is null', as_on).order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    elsif due_status.upcase == 'FUTURE' and completion_status.upcase == 'COMPLETE'
+      InvoicingMilestone.where('due_date > ? and completion_date is not null', as_on).order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    elsif due_status.upcase == 'FUTURE' and completion_status.upcase == 'ALL'
+      InvoicingMilestone.where('due_date > ?', as_on).order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    elsif due_status.upcase == 'ALL' and completion_status.upcase == 'INCOMPLETE'
+      InvoicingMilestone.where('completion_date is null').order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    elsif due_status.upcase == 'ALL' and completion_status.upcase == 'COMPLETE'
+      InvoicingMilestone.where('completion_date is not null').order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    elsif due_status.upcase == 'ALL' and completion_status.upcase == 'ALL'
+      InvoicingMilestone.all.order('due_date').each do |im|
+        details = {}
+        details['id'] = im.id
+        details['name'] = im.name
+        details['amount'] = im.amount
+        details['uninvoiced'] = im.uninvoiced
+        details['remaining'] = im.amount - im.uninvoiced
+        details['due_date'] = im.due_date.to_s
+        details['last_reminder_date'] = im.last_reminder_date
+        details['completion_date'] = im.completion_date.to_s
+        details['project'] = im.project.name
+        details['client'] = im.project.pipeline.client.name
+        details['business_unit'] = im.project.business_unit_name
+        data << details
+      end
+    end
+    data
+  end
 end
