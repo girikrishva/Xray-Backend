adc5fbb41d704fe2ac7472070832553fb148f6e2
baput <bapu09tavildar@gmail.com>
2017-02-08 19:56:55 +0530

    Added new UI

--- app/admin/resource_utilization.rb
+++ app/admin/resource_utilization.rb
@@ -87,12 +87,16 @@ config.batch_actions = false
     # end
      render partial: "resource_utilization"
     div class:"popup","data-popup": "popup-1" do
-      div class:"popup-inner",style:"overflow : auto;" do
-        span class:"ajax_content" do 
-        end
-          a class:"popup-close","data-popup-close":"popup-1","href":"#" do
-            "Close"
+      div class:"popup-inner" do
+          div class:"modal-header" do 
+            a class:"popup-close","data-popup-close":"popup-1","href":"#" do
+              "X"
+            end
           end
+        div class:"ajax_content_container" do 
+          div class:"ajax_content" do 
+          end
+        end
       end
     end
   end
--- app/assets/javascripts/pipeline_forecast.js
+++ app/assets/javascripts/pipeline_forecast.js
@@ -1,14 +1,42 @@
 $(document).ready(function() {
+ $( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd' });
+	$("#index_table_pipeline_forecasts,.pagination_information,.pagination").hide()
+   var fullDate = new Date()
+   // on change start
+         $("#datepicker").on('change',function(){
+        currentDate = $(this).val()
+        responce  = ajax_call("api/pipeline_for_all_statuses?as_on="+currentDate)
+    var html_value = ""
+ $("#initial_table").html(table_view(responce,html_value))
+           $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+        $(".dropdown_menu").toggle()
+        e.preventDefault();
+    });
  
-	// $(".col-expected_start,.col-pipeline_status_id,.hidden,.paginated_collection").hide()
-  responce  = ajax_call("api/pipeline_for_status?status_id=100&as_on="+"2017-01-01"+"&with_details=true")
-  $("#initial_table").html(convert_ajax_to_htm(responce))
-	$(".text_link").on('click',function(){
-		var status_id = $(this).closest('tr').find('td:eq(-1)').text()
-		var as_on = $(this).find(".hidden").html() + "-01"
-		responce  = ajax_call("api/pipeline_for_status?status_id="+status_id+"&as_on="+as_on+"&with_details=true")
-		var html_value = "<span id='close' style='cursor:pointer' hidden></span>"+convert_ajax_to_htm(responce)
-		$.each( responce["data"], function( index, value ){
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(10);
+        $(".dropdown_menu").toggle()
+        e.preventDefault();
+    });
+  
+      })
+      $("#datepicker").val(currentDate)
+   // on change end
+  var twoDigitMonth = ((fullDate.getMonth().length+1) === 1)? (fullDate.getMonth()+1) : '0' + (fullDate.getMonth()+1);
+  var currentDate = fullDate.getDate() + "/" + twoDigitMonth + "/" + fullDate.getFullYear();
+  responce  = ajax_call("api/pipeline_for_all_statuses?as_on="+currentDate)
+  html_value = ""
+ 
+  $("#initial_table").html(table_view(responce,html_value))
+  $(".text_link").on('click',function(){
+  status_id = $("."+$(this).parent().find(".pipeline_status").html()).attr('id')
+  as_on = $(this).attr('class').split(" ")[0]
+    responce  = ajax_call("api/pipeline_for_status?status_id="+status_id+"&as_on="+as_on+"&with_details=true")
+    var html_value = "<span id='close' style='cursor:pointer' hidden></span>"+convert_ajax_to_htm(responce)
+    $.each( responce["data"], function( index, value ){
                 if (index  ==  0){
                   html_value += convert_ajax_to_htm(value["pipeline"]).replace("</table>","")
                 }
@@ -18,30 +46,35 @@ $(document).ready(function() {
             });
             html_value += "</table>"
 		$(".ajax_content").html(html_value)
-		$(".created_at,.updated_at,.deleted_at,.comments,.data").hide()
+     $.each($("td.expected_value,td.total_pipeline"), function( index1, value1 ){
+     $(this).text("$"+$(this).text())
+      })
+		$(".created_at,.updated_at,.deleted_at,.comments,.data,.update_by,.pipeline_status_id").hide()
 	})
 
 	      function return_only_t_data(responce){
              var ajax_content_value = "</tr><tr>"
              $.each(responce, function(key, value) {
-                  ajax_content_value +="<td class='"+key.replace(" ", "_")+"'style = 'border: 1px solid black;' >"+value+"</td>" 
+                  ajax_content_value +="<td class='"+key.replace(" ", "_")+"'style = 'border-top: 1px solid #ddd;' >"+value+"</td>" 
                   });
            ajax_content_value +="</tr>"
            return ajax_content_value
           }
 
           function convert_ajax_to_htm(responce){
-   var ajax_content_value = "<table><tr>"
+   var ajax_content_value = "<table class = 'table table-striped'><tr>"
            $.each(responce, function(key, value) {
-                  ajax_content_value +="<th class='"+key.replace(" ", "_")+" 'style = 'border: 1px solid black;'>"+key.replace(/_/g," ").toUpperCase()+"</th>" 
+                  ajax_content_value +="<th class='"+key.replace(" ", "_")+" 'style = 'border-top: 1px solid #ddd;'>"+key.replace(/_/g," ").toUpperCase()+"</th>" 
                   });
            ajax_content_value += "</tr><tr>"
              $.each(responce, function(key, value) {
-                  ajax_content_value +="<td class='"+key.replace(" ", "_")+"'style = 'border: 1px solid black;' >"+value+"</td>" 
+                  ajax_content_value +="<td class='"+key.replace(" ", "_")+"'style = 'border-top: 1px solid #ddd;' >"+value+"</td>" 
                   });
            ajax_content_value +="</tr></table>"
            return ajax_content_value
           }
+
+
 
 	function ajax_call(url) {
 	    var responce = ""
@@ -72,3 +105,47 @@ $(document).ready(function() {
     });
 });
 })
+var monthNames = [ "Jan",   "Feb", "Mar",    "Apr",
+                   "May",       "Jun",     "Jul",     "Aug",
+                   "Sept", "Oct",  "Nov", "Dec" ]
+function table_view(responce,html_value){
+   $.each( responce, function( index, value ){
+                if (index  ==  0){
+                  html_value = "<table class ='table table-striped'><tr class ='table-row-header'>"
+           $.each(value, function(key, value) {
+                  key1 = key
+                  if (typeof value == 'object'){ 
+                    key1 = new Date(key)
+                    key1 = key1.getFullYear()+"-" + monthNames[key1.getMonth()]
+                     }
+                     else{
+                      key1 =key.replace(/_/g," ").toUpperCase()
+                     }
+                  html_value +="<th class='"+key.replace(" ", "_")+" 'style = 'border-top: 1px solid #ddd;'>"+key1+"</th>" 
+                  });
+           html_value += "</tr><tr>"
+             $.each(value, function(key, value) {
+                  other_class = ""
+                  attr = ""
+                  if (value["total_pipeline"] > 0){other_class=" text_link"}
+                  if (typeof value == 'object'){ 
+                    value = "$"+value["total_pipeline"]
+                  attr = "data-popup-open='popup-1'"
+                     }
+                  html_value +="<td class='"+key.replace(" ", "_")+other_class+"'style = 'border-top: 1px solid #ddd;'"+attr+" >"+value+"</td>" 
+                  });
+           html_value +="</tr>"
+                }
+                else{
+               html_value += "</tr><tr>"
+             $.each(value, function(key, value) {
+                  other_class = ""
+                  if (value["total_pipeline"] > 0){other_class=" text_link"}
+                  if (typeof value == 'object'){ value = "$"+value["total_pipeline"] }
+                  html_value +="<td class='"+key.replace(" ", "_")+other_class+"'style = 'border-top: 1px solid #ddd;'"+attr+" >"+value+"</td>" 
+                  });
+           html_value +="</tr>"
+                }
+            });
+                return html_value
+}
