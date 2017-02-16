8eaa2cc1517b8b86842ffe51e9eab7accf48143e
girikrishav <girikrishva@gmail.com>
2017-02-16 08:51:59 +0530

    Optimized code for 'Resource Performance' report (interim).

--- app/models/admin_users_audit.rb
+++ app/models/admin_users_audit.rb
@@ -51,13 +51,16 @@ class AdminUsersAudit < ActiveRecord::Base
   def self.active_users_outflow(from_date, to_date)
     from_date = Date.parse(from_date)
     to_date = Date.parse(to_date)
-    with_details = (with_details.to_s == 'true') ? true : false
     data = {}
     lower_date = from_date
     (from_date..to_date).each do |d|
-      if (d - lower_date) > 22
-        lower_date = Date.parse(d.year.to_s + '-' + d.month.to_s + '-' + '1'.to_s)
-        next
+      if d > from_date
+        if d == d.end_of_month or d == to_date
+          days_in_month = [d - lower_date + 1, Rails.configuration.max_work_days_per_month].min
+          lower_date = Date.parse((d + 1).year.to_s + '-' + (d + 1).month.to_s + '-' + '1'.to_s)
+        else
+          next
+        end
       end
       month_year_key = AdminUsersAudit.month_year(d)
       AdminUsersAudit.active_users(d).each do |aua|
@@ -71,7 +74,7 @@ class AdminUsersAudit < ActiveRecord::Base
           data[user_key]['date_of_joining'] = aua.date_of_joining.to_s
           data[user_key]['date_of_leaving'] = aua.date_of_leaving.to_s
           data[user_key]['active'] = aua.active.to_s
-          amount = aua.cost_rate * Rails.configuration.max_work_hours_per_day
+          amount = days_in_month * aua.cost_rate * Rails.configuration.max_work_hours_per_day
           if !data[user_key].has_key?('user_total')
             data[user_key]['user_total'] = amount
           else
