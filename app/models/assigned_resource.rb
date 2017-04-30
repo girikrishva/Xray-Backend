f5b2c199bfd6419335f990942de5313a2adb1b17
girikrishav <girikrishva@gmail.com>
2017-04-30 19:40:19 +0530

    Added rescue to prevent crash.

--- app/models/assigned_resource.rb
+++ app/models/assigned_resource.rb
@@ -46,7 +46,7 @@ class AssignedResource < ActiveRecord::Base
   end
 
   def assigned_resource_name
-    'Id: [' + self.id.to_s + '], Client: [' + self.project.pipeline.client.name + '], Project: [' + self.project.name + '], Resource: [' + self.resource.resource_name + '], Skill: [' + resource.skill_name + '], Start Date: [' + self.start_date.to_s + '], End Date: [' + self.end_date.to_s + '], Hours Per Day: [' + self.hours_per_day.to_s + ']'
+    'Id: [' + self.id.to_s + '], Client: [' + self.project.pipeline.client.name + '], Project: [' + self.project.name + '], Resource: [' + self.resource.resource_name + '], Skill: [' + resource.skill_name + '], Start Date: [' + self.start_date.to_s + '], End Date: [' + self.end_date.to_s + '], Hours Per Day: [' + self.hours_per_day.to_s + ']' rescue nil
   end
 
   def over_assignment_check
