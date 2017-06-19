5584b2eda663cb15df61aed7781b33e92280c845
Yaswant Singh <yaswant.s@sigmainfo.net>
2017-06-19 19:09:58 +0530

    Adding pipeline and grossprofit feture

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
         url: "/admin/api/resource_costs_panel_data",
         context: document.body
     }).done(function (data) {
+        console.log("RC===="+JSON.stringify(data))
         $.each(data.datasets[0].data, function (index, value) {
             data_array1[index] = value
         });
@@ -89,7 +90,71 @@ $(document).ready(function () {
 
     });
 
+    $.ajax({
+        url: "/admin/api/gross_profit_panel_data",
+        context: document.body
+    }).done(function (data) {
+        var gross_p_data = {
+            labels : data.labels,
+            datasets : [
+                {
+                    pointColor : "#6495ED",
+                    strokeColor : "#6495ED",
+                    pointStrokeColor : "#6495ED",
+                    data : data.datasets[0].data,
+                    title : "Gross Profit"
+                }
+            ]
+        }
+        new Chart(document.getElementById("grass_profit_panel").getContext("2d")).Line(gross_p_data,gp_struct);
+    });
 
+
+    $.ajax({
+        url: "/admin/api/pipeline_by_business_unit_panel_data",
+        context: document.body
+    }).done(function (data) {
+        console.log("pipeline===="+JSON.stringify(data))
+//        $.each(data.datasets[0].data, function (index, value) {
+//            resource_data1[index] = value
+//        });
+//        $.each(data.datasets[1].data, function (index, value) {
+//            resource_data2[index] = value
+//        });
+
+        bench_dest_data = {
+            labels:  ["April", "May", "June"],
+            datasets: [
+                {
+                    fillColor: "#23457d",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointstrokeColor: "yellow",
+                    data: [20,30,40],
+                    title: "CCI"
+                },
+                {
+                    fillColor: "#D2691E",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "green",
+                    pointstrokeColor: "yellow",
+                    data: [20,30,40],
+                    title: "CCS"
+                },
+                {
+                    fillColor: "#FFC200",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "green",
+                    pointstrokeColor: "yellow",
+                    data: [20,30,40],
+                    title: "CCUS"
+                }
+            ]
+        }
+
+        new Chart(document.getElementById("pipeline_chart").getContext("2d")).StackedBar(bench_dest_data, opt1);
+
+    });
 
 });
 
@@ -108,6 +173,8 @@ function fctMouseDownLeft(event, ctx, config, data, other) {
     if(other != null){
         $("#dialog1").dialog('open');
     }
+
+    console.log("=========data.datasets[other.v11].title===="+data.datasets[other.v11].title)
 
     graph_type = data.datasets[other.v11].title
     graph_month = data.labels[other.v12]
@@ -130,11 +197,19 @@ function fctMouseDownLeft(event, ctx, config, data, other) {
         cost_s_url ="/admin/api/bench_counts_by_skill_panel_data?as_on="
         cost_d_url ="/admin/api/bench_counts_by_designation_panel_data?as_on="
     }
+    else if (data.datasets[other.v11].title == "Gross Profit") {
+        cost_s_url ="/admin/api/gross_profit_by_business_unit_panel_data?as_on="
+        cost_d_url ="/admin/api/gross_profit_versus_indirect_cost_panel_data?as_on="
+    }else if(["CCI","CCS","CCUS"].indexOf(data.datasets[other.v11].title)){
+
+    }
     as_on = '2017-06-10'
     $.ajax({
         url: cost_s_url + as_on,
         context: document.body
     }).done(function (data) {
+
+        console.log("test data skill"+JSON.stringify(data))
         mydata3 = {
             labels: data.labels,
             datasets: [
@@ -167,6 +242,16 @@ function fctMouseDownLeft(event, ctx, config, data, other) {
         url: cost_d_url + as_on,
         context: document.body
     }).done(function (data) {
+        console.log("test data datasets..........."+JSON.stringify(data.datasets[0].data))
+        var d_lable=''
+        var d_show=false
+        if (graph_type == "Gross Profit"){
+            d_lable="Profits Vs Indirect Costs for the current month"
+            d_show=true
+        }else{
+            d_lable=graph_type + " Details by Designation"
+            d_show=false
+        }
         dis_data = {
             labels: data.labels,
             datasets: [
@@ -184,15 +269,34 @@ function fctMouseDownLeft(event, ctx, config, data, other) {
         mydata1.shapesInChart = [];
         dis_opt = {
             canvasBorders: true,
-            graphTitle: graph_type + " Details by Designation",
+            graphTitle: d_lable ,
             legend: true,
             inGraphDataShow: false,
             annotateDisplay: true,
             graphTitleFontSize: 18,
-            barValueSpacing: 40
-
+            inGraphDataShow : true
         }
-        var myStackedBar = new Chart(document.getElementById("canvas_Bar4").getContext("2d")).StackedBar(dis_data, dis_opt);
+
+        var gp_pie_data = {
+            labels: [""],
+            datasets: [
+                {
+                    data: [data.datasets[0].data[0]],
+                    fillColor: "#D2691E",
+                    title: "Indirect Costs"
+                },
+                {
+                    data: [data.datasets[0].data[1]],
+                    fillColor: "#6495ED",
+                    title: "Gross Profits,"
+                }
+            ]
+        }
+            if (graph_type == "Gross Profit"){
+            var myStackedBar = new Chart(document.getElementById("canvas_Bar4").getContext("2d")).Pie(gp_pie_data, dis_opt);
+        }else{
+            var myStackedBar = new Chart(document.getElementById("canvas_Bar4").getContext("2d")).StackedBar(dis_data, dis_opt);
+        }
     });
 }
 
@@ -214,6 +318,27 @@ var opt1 = {
     graphTitleFontSize: 18,
     barValueSpacing: 40,
     mouseDownLeft: fctMouseDownLeft
+}
+
+var gp_struct = {
+    canvasBorders : true,
+    canvasBordersWidth : 3,
+    canvasBordersColor : "black",
+    datasetFill : false,
+    graphTitle : " ",
+    graphTitleFontSize: 18,
+    graphMin : 2500000,
+    yAxisMinimumInterval : 5,
+    bezierCurve: false,
+    annotateDisplay : true,
+    inGraphDataShow : true,
+    mouseDownLeft : fctMouseDownLeft
+//    mouseDownRight : fctMouseDownRight,
+//    mouseDownMiddle : fctMouseDownMiddle,
+//    mouseMove : fctMouseMove,
+//    mouseOut : fctMouseOut
+
+
 }
 
 
