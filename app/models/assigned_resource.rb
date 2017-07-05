6938c2989532c6e78c231c94ca3bdf68e342feef
girikrishav <girikrishva@gmail.com>
2017-07-05 16:42:46 +0530

    Interim stable commit.

--- app/models/assigned_resource.rb
+++ app/models/assigned_resource.rb
@@ -111,7 +111,7 @@ class AssignedResource < ActiveRecord::Base
   end
 
   def assignment_cost(as_on)
-    self.assignment_hours(as_on) * self.cost_rate
+    self.assignment_hours(as_on) * AdminUser.find(self.resource.admin_user.id).cost_rate
   end
 
   private
