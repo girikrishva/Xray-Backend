5964a396ad0a9ecdd9b2f9fea19d85a5168bc5cc
Giridhar K <girikrishva@gmail.com>
2017-07-30 14:48:34 +0530

    Local stable commit.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your site routed with "root".
   # root 'welcome#index'
   get '/admin/project_pop_up' => 'admin/projects#pop_up_view'
   get '/admin/get_team' => 'admin/organization_charts#get_users'
